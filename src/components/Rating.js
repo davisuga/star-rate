3adb1a72897e0f4d79f2d16e0b6dddff4cfee6a2
Davi Suga <daviciencia1@gmail.com>
2022-12-09 19:02:16 -0300

    fix: fixed closure bug

--- src/components/Rating.js
+++ src/components/Rating.js
@@ -1,5 +1,5 @@
 //@ts-check
-import React, { useState } from "react";
+import React, { useEffect, useRef, useState } from "react";
 import "./Rating.css";
 const def = {
   // 0.5 to 5
@@ -10,31 +10,32 @@ const def = {
   steps: 0.5,
 };
 
-// Utility function to calculate if the mouse event happened on the left side of the target or the right side.
-const isLessThanHalf = (event) => {
-  const { target } = event;
-  const boundingClientRect = target.getBoundingClientRect();
-  let mouseAt = event.clientX - boundingClientRect.left;
-  mouseAt = Math.round(Math.abs(mouseAt));
-  const half = mouseAt <= boundingClientRect.width / 2;
-  console.log({ half });
-  return half;
-};
+// const calcIncrement = ()
 
 const Rating = (props = def) => {
   const emptyIcon = props.emptyIcon || "/icons/stars/empty.svg";
   const filledIcon = props.filledIcon || "/icons/stars/filled.svg";
   const halfFilledIcon = props.halfFilledIcon || "/icons/stars/half.svg";
-
-  const showHalfSteps = props.steps % 1 === 0.5;
-
   const [savedValue, setSavedValue] = useState(props.value);
   const [shownValue, setShownValue] = useState(savedValue);
 
-  const calcNewValue = (index) => (e) =>
-    index + showHalfSteps && isLessThanHalf(e) ? 1.5 : 1;
+  const showHalfSteps = props.steps % 1 === 0.5;
+
+  const isLessThanHalf = (event) => {
+    const { target } = event;
+    const boundingClientRect = target.getBoundingClientRect();
+    let mouseAt = event.clientX - boundingClientRect.left;
+    mouseAt = Math.round(Math.abs(mouseAt));
+    const half = mouseAt <= boundingClientRect.width / 2;
+    console.log({ half });
+    return half;
+  };
+
+  // Utility function to calculate if the mouse event happened on the left side of the target or the right side.
 
   const renderSymbol = (symbolVal, index) => {
+    const getInc = (e) => (showHalfSteps && isLessThanHalf(e) ? 1.5 : 1);
+
     return (
       <img
         src={
@@ -48,10 +49,10 @@ const Rating = (props = def) => {
         data-testid="rating-icon"
         alt="Rate"
         onMouseMove={(e) => {
-          setShownValue(calcNewValue(index)(e));
+          setShownValue(index + getInc(e));
         }}
         onClick={(e) => {
-          setSavedValue(calcNewValue(index)(e));
+          setSavedValue(index + getInc(e));
         }}
       />
     );
